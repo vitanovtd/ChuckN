baee724e0669ae8b159d73f25f8c5e3dd2b14e4f
Tihomir Vitanov <vitanovtd@gmail.com>
2024-10-06 21:44:21 +0300

    updated favourites cards layout and logic

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -2,12 +2,32 @@
   <div class="favourites">
     <h2>Your Jokes Tuk!</h2>
     <div class="row">
-      <div v-if="favourites.length === 0"> No favourite jokes have been selected</div>
-      <div class="col" v-for="joke in favourites" :key="joke.id">
-        <div class="card mb-3">
-          <div class="card-body">
-            <p class="card-text">{{ joke.value }}</p>
-            <button class="btn btn-warning" @click="removeFromFavourites(joke.id)">Remove <i :class="'bi bi-star-fill'"></i> </button>
+      <div v-if="favourites.length === 0">
+        No favourite jokes have been selected
+      </div>
+      <div
+        class="col-sm-6 col-md-4 col-lg-3 mb-4"
+        v-for="joke in favourites"
+        :key="joke.id"
+      >
+        <div class="card h-100">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <router-link
+              :to="`/jokes/${joke.id}`"
+              class="card-text text-decoration-none text-dark mb-3"
+            >
+              {{
+                joke.value.length > 100
+                  ? joke.value.substring(0, 100) + "..."
+                  : joke.value
+              }}
+            </router-link>
+            <button
+              class="btn btn-warning mt-auto remove-button"
+              @click="removeFromFavourites(joke.id)"
+            >
+              Remove <i class="bi bi-star-fill"></i>
+            </button>
           </div>
         </div>
       </div>
@@ -16,25 +36,23 @@
 </template>
 
 <script lang="ts">
-import { computed } from 'vue';
-import { useFavouritesStore } from '../stores/favouritesStore';
+import { computed } from "vue";
+import { useFavouritesStore } from "../stores/favouritesStore";
 
 export default {
-  name: 'Favourites',
+  name: "Favourites",
   setup() {
     const favouritesStore = useFavouritesStore();
-    // Use computed to watch the changes reactively
     const favourites = computed(() => favouritesStore.favourites);
-    console.log(favouritesStore.favourites);
 
     return {
       favourites,
       removeFromFavourites: favouritesStore.removeFromFavourites,
     };
-  }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  @import '@/styles/Favourites.scss';
+@import "@/styles/Favourites.scss";
 </style>
